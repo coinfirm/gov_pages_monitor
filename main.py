ea9f5b139c3b61f3f32e19569c7bca3f19a01275
michallachowskiCoinfirm <michal.lachowski@coinfirm.com>
2022-04-13 16:32:04 +0200

    Refactoring

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ import random
 import time
 import datetime as DT
 
-s = Service('./chromedriver')
+chromedriverPath = '/home/michal-lachowski/PycharmProjects/pythonProject/firstTask/chromedriver'
+s = Service(chromedriverPath)
 driver = webdriver.Chrome(service=s)
 
 
-def make_url(para):
-    return "https://www.instantmarkets.com" + str(para)
-
-
 def xpath(elem):
+
     url = f'https://www.instantmarkets.com/q/{elem}?ot=Bid%20Notification,Pre-Bid%20Notification,Award,Contract&pg=1&pdf=0-7&c=us&os=Active,Archived'
     page = requests.get(url)
     bs = BeautifulSoup(page.content, 'html.parser')
@@ -31,10 +29,12 @@ def xpath(elem):
 
 
 def count_number_of_pages(number):
+
     return math.ceil(number / 10)
 
 
 def instant_markets():
+
     keywords = ['crypto', 'cryptocurrency', 'crypto_currency',
                 'virtual_currency', 'bitcoin', 'ethereum',
                 'virtual_asset', 'blockchain', 'TRM']
@@ -47,7 +47,7 @@ def instant_markets():
             bs = BeautifulSoup(page.content, 'html.parser')
             links = bs.findAll('a', class_='large default-text opptitle ng-star-inserted')
             for el in links:
-                href = make_url(el['href'])
+                href = "https://www.instantmarkets.com" + str(el['href'])
                 title = el['title']
                 payload = {
                     "text": "New article found on https://www.instantmarkets.com/ with key word: " + elem + " ```" + "Title: " + title + " \n" + "Link: "
@@ -56,20 +56,24 @@ def instant_markets():
                                          "/aee3kf3xwgnMr0Nq5jKxwXFW", json.dumps(payload))
 
 
+def wait_for_content():
+    wait_time = random.randint(2, 6)
+    time.sleep(wait_time)
+
 def get_number_of_pages(text):
+
     first_split = text.split('(')
     sec_split = first_split[1].split(')')
     return sec_split[0]
 
 
 def previous_weak_date(number):
+
     today = DT.date.today()
     week_ago = today - DT.timedelta(days=number)
-    str_week_ago = str(week_ago)
-    date = str_week_ago.split('-')
-    year = date[0]
-    month = date[1]
-    day = date[2]
+    year = str(week_ago.year)
+    month = str(week_ago.month)
+    day = str(week_ago.day)
     day_name = week_ago.strftime("%A")[0:3]
     month_name = week_ago.strftime("%B")
     month_name_short = week_ago.strftime("%b")
@@ -77,6 +81,7 @@ def previous_weak_date(number):
 
 
 def ec_europa():
+
     keywords = ['crypto', 'cryptocurrency', 'crypto currency',
                 'virtual currency', 'bitcoin', 'ethereum',
                 'virtual asset', 'blockchain', 'TRM']
@@ -87,8 +92,7 @@ def ec_europa():
             url = f'https://ec.europa.eu/commission/presscorner/advancedsearch/en?keywords={elem}&dotyp=&parea=&pareaType=&datepickerbefore=&datebefore=&commissioner=&datepickerafter={day}%20{month_name}%20{year}&dateafter={day_name}%20{month_name_short}%20{day}%20{year}%2000:00:00%20GMT%2B0200%20(Central%20European%20Summer%20Time)&pagenumber=1'
 
             driver.get(url)
-            wait_time = random.randint(2, 6)
-            time.sleep(wait_time)
+            wait_for_content()
             number = driver.find_element(By.CLASS_NAME, "ecl-heading.ecl-heading--h2.ecl-u-mb-m")
             try:
                 num = number.find_element(By.TAG_NAME, "span").text
@@ -97,8 +101,7 @@ def ec_europa():
                 for i in range(number_of_pages):
                     url2 = f'https://ec.europa.eu/commission/presscorner/advancedsearch/en?keywords={elem}&dotyp=&parea=&pareaType=&datepickerbefore=&datebefore=&commissioner=&datepickerafter={day}%20{month_name}%20{year}&dateafter={day_name}%20{month_name_short}%20{day}%20{year}%2000:00:00%20GMT%2B0200%20(Central%20European%20Summer%20Time)&pagenumber=1'
                     driver.get(url2)
-                    wait_time = random.randint(2, 6)
-                    time.sleep(wait_time)
+                    wait_for_content()
                     by_class = driver.find_elements(By.CLASS_NAME, "ecl-link.ecl-list-item__link")
                     for ele in by_class:
                         href = ele.get_attribute('href')
@@ -110,10 +113,7 @@ def ec_europa():
                                                  "/aee3kf3xwgnMr0Nq5jKxwXFW", json.dumps(payload))
             except:
                 continue
-                # payload = {
-                #     "text": "On website https://ec.europa.eu/ with key word: " + elem + " ```" + "No new matches found" + " ```"}
-                # response = requests.post("https://hooks.slack.com/services/T14NAEEBZ/B038S999Y3W"
-                #                          "/aee3kf3xwgnMr0Nq5jKxwXFW", json.dumps(payload))
+
     except:
         return False
 
@@ -121,6 +121,7 @@ def ec_europa():
 
 
 def last_7_days():
+
     list_of_days = []
     for days in range(7):
         year, month, day, day_name, month_name_short, month_name = previous_weak_date(days)
@@ -131,22 +132,18 @@ def last_7_days():
 
 
 def sam_gov():
+
     keywords = ['crypto', 'cryptocurrency', 'crypto currency',
                 'virtual currency', 'bitcoin', 'ethereum',
                 'virtual asset', 'blockchain', 'TRM']
 
     for elem in keywords:
-        # url = f'https://sam.gov/search/?page=1&pageSize=25&sort=-modifiedDate&sfm%5Bstatus%5D%5Bis_active%5D=true&sfm' \
-        #       f'%5BsimpleSearch%5D%5BkeywordRadio%5D=EXACT&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bkey%5D' \
-        #       f'={elem}&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bvalue%5D={elem} '
-
         url = f'https://sam.gov/search/?page=1&pageSize=25&sort=-modifiedDate&sfm%5Bstatus%5D%5Bis_active%5D=true&sfm' \
               f'%5BsimpleSearch%5D%5BkeywordRadio%5D=EXACT&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bkey%5D' \
               f'={elem}&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bvalue%5D={elem} '
 
         driver.get(url)
-        wait_time = random.randint(2, 6)
-        time.sleep(wait_time)
+        wait_for_content()
         data_list = []
         try:
             page = driver.find_element(By.CLASS_NAME, 'sds-pagination__total').text.split(' ')
@@ -158,8 +155,7 @@ def sam_gov():
                        f'={elem}&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bvalue%5D={elem} '
 
                 driver.get(url2)
-                wait_time = random.randint(2, 6)
-                time.sleep(wait_time)
+                wait_for_content()
 
                 date = driver.find_elements(By.XPATH, '//*[@class="sds-field sds-field--stacked"]/div[2]/span')
 
